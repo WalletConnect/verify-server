51ec9a52655266d6702ca2eec1cbe46460379693
Gancho Radkov <ganchoradkov93@gmail.com>
2023-01-31 10:44:39 +0200

    feat: updates gist url to include `/attestation` for POST req

--- src/handlers/enclave.rs
+++ src/handlers/enclave.rs
@@ -13,7 +13,7 @@ pub async fn handler(
 ) -> impl IntoResponse {
     match project_id.as_str() {
         "index.js" => {
-            let resp = reqwest::get("https://gist.githubusercontent.com/ganchoradkov/85f747268696d2b7585292b0b40f9d43/raw/328199c208ad09faf33f4db55e9fdd36e75506bb/index.js")
+            let resp = reqwest::get("https://gist.githubusercontent.com/ganchoradkov/85f747268696d2b7585292b0b40f9d43/raw/a38da04be479d272c7947005da2f374949952913/index.js")
                 .await.unwrap()
                 .text()
                 .await.unwrap();
