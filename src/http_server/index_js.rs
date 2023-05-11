029e130a2366607de123cd798264c10f86e3ba2e
xDarksome <dev@darkso.me>
2023-05-11 11:57:06 +0400

    fix: don't post /attestation if attestationId is invalid (#28)

--- src/http_server/index_js.rs
+++ src/http_server/index_js.rs
@@ -6,6 +6,7 @@ window.addEventListener("message", (event) => {
     const attestationId = event.data
     const origin = event.origin
     if (!attestationId) return
+    if (attestationId.length !== 64) return
     fetch(`${window.location.protocol}//${window.location.host}/attestation`, {
         method: "POST",
         body: JSON.stringify({ attestationId, origin }),
