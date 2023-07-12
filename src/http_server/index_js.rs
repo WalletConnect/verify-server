03da3228c027cea0708ff63a949412c389fa35fa
Darksome <dev@darkso.me>
2023-07-12 20:00:29 +0400

    feat: update JS

--- src/http_server/index_js.rs
+++ src/http_server/index_js.rs
@@ -1,6 +1,11 @@
 use axum::response::{Html, IntoResponse};
 
 const INDEX_JS: &str = r#"
+const getCookie = (name) => {
+    const value = `${document.cookie}`
+    const parts = value.split(`${name}=`)
+    return parts.pop().split(';').shift()
+}
 // event subscribed by Verify Enclave
 window.addEventListener("message", (event) => {
     const attestationId = event.data
@@ -10,8 +15,17 @@ window.addEventListener("message", (event) => {
     fetch(`${window.location.protocol}//${window.location.host}/attestation`, {
         method: "POST",
         body: JSON.stringify({ attestationId, origin }),
-        headers: new Headers({ 'content-type': 'application/json' })
+        headers: new Headers({ 
+            'content-type': 'application/json',
+            'x-csrf-token': getCookie('sid')
+        })
     })
+})
+// auto reload to refresh cookie
+window.addEventListener("load", async () => {
+    setInterval(() => {
+        window.location.reload()
+    }, 60_000)
 })"#;
 
 pub async fn get() -> impl IntoResponse {
