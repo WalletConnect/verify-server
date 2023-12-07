e6d80b646bb074006a63629fa1971cf8888fe999
Xavier Basty <xavier@walletconnect.com>
2023-12-07 17:43:47 +0100

    remove legacy files

--- terraform/ecs/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "load_balancer_arn" {
-  value = aws_lb.application_load_balancer.arn
-}
--- terraform/redis/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "endpoint" {
-  value = aws_elasticache_cluster.cache.cache_nodes[0].address
-}
-
-output "cluster_id" {
-  value = aws_elasticache_cluster.cache.id
-}
